0ffce11355e53be936bcbe33e7d3565c92d6fb3d
U-BSD\485716 <485716@SHS-HJ98ND2.beaverton.k12.or.us>
2023-01-24 12:15:41 -0800

    Second Commit

--- main.cpp
+++ main.cpp
@@ -10,6 +10,50 @@ void printAverage(Node* n);
 void deleteStudent(Node* &head, int id);
 
 int main() {
-  
+
+  Node* node = NULL;
+
+  int running = 1;
+
+  string in;
+  int id = 0;
+
+  while (running == 1) {
+
+    cout << "enter command: [ADD,PRINT,DELETE,AVERAGE,QUIT]" << endl;
+    cin >> in;
+    if (in == "ADD" || in == "Add" || in == "add") {
+      Student* s = new Student;
+      s -> makeStudent();
+      Node* n = new Node;
+      n -> setStudent(s);
+
+      addStudent(Head,n);
+    }
+    if (in == "PRINT" || in == "Print" || in == "print") {
+      printALL(Head);
+    }
+    if (in == "DELETE" || in == "Delete" || in == "delete") {
+      if (Head == NULL) {
+	cout << "Student not in list" << endl;
+      }
+      else {
+	cout << "Enter ID to be deleted: " << endl;
+	cin >> id;
+	cin.ignore();
+
+	deleteStudent(Head,id);
+
+      }
+    }
+    if (in == "AVERAGE" || in == "Average" || in == "average") {
+      printAverage(Head);
+    }
+    if ( in == "QUIT" || in == "Quit" || in == "quit") {
+      running = 0;
+    }
+
+
+  }
   
 }
